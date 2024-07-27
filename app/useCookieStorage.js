fd2230a68e553721f370e0bb076ddcfbf38a2589
Luís Copetti <luis@mana.app>
2024-07-27 20:00:37 -0300

    Create custom hook for game state storage

---
+++ app/useCookieStorage.js
@@ -0,0 +1,32 @@
+
+import { useState } from 'react';
+import { useCookies } from 'react-cookie';
+import { useEffect } from 'react';
+
+const cookieStorageKey = 'game-storage-key';
+
+const useCookieStorage = () => {
+
+    const [loading, setLoading] = useState(true);
+    const [cookies, setCookie] = useCookies([cookieStorageKey]);
+
+    useEffect(() => {
+        setTimeout(() => { setLoading(false) }, 1000);
+    }, []);
+
+
+    const save = (data) => {
+        const content = JSON.stringify(data);
+        setCookie(cookieStorageKey, content);
+    }
+
+    const retrieve = () => cookies[cookieStorageKey];
+
+    return {
+        loading,
+        retrieve,
+        save,
+    }
+}
+
+export default useCookieStorage;
